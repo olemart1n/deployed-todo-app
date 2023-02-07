57167521bcddab0c1c27774c00d4220f33e467b0
Ole Martin <91548973+olemart1n@users.noreply.github.com>
2023-02-07 23:17:52 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
     password: process.env.PASSWORD,
     host: process.env.HOST,
     port: process.env.DBPORT,
-    database: "todo",
+    database: "todoapp",
 });
 
 module.exports = pool;
